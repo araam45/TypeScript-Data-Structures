271eee976a9831966eab7498caccba4691add1d5
Araam Zaremehrjardi <araamzaremehrjardi@gmail.com>
2020-05-14 01:37:11 -0700

    Adding Stack Data Structure

---
+++ __ds_stack.ts
@@ -0,0 +1,62 @@
+﻿/*  Created by Araam Zaremehrjardi.
+ *  
+ *  --- BEGINNING OF NOTES [0] ---
+ *  FORMAT OF CLASS NAME IS AS FOLLOWS...
+ *  __(class-notator)_(name of class)
+ *  "__" signifies class is user-made.
+ *  "(class-notator)" signifies if the class is ds (data structure), h (helper), t (thing), m (miscellaneous).
+ *  "_(name of class)" signifies the name of the class.
+ *  --- END OF NOTES [0] ---
+ *
+ *  --- BEGINNING OF NOTES [1] ---
+ *  THE DATA STRUCTURE IS VISUALIZED AS FOLLOWS...
+ *  [0, 1, 2...X]
+ *             ^
+ *  Items are added and removed from this location.
+ *  --- END OF NOTES [1] ---
+ *  
+ */
+
+class __ds_stack<itemType> {
+
+    private m_stack: Array<itemType> = new Array<itemType>();
+
+    public isEmpty(): boolean {
+
+        if (this.m_stack.length == 0) {
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+    public push(item: itemType): boolean {
+
+        if (this.m_stack.length < 1000) {
+
+            this.m_stack.push(item);
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public pop(): boolean {
+
+        this.m_stack.splice(this.m_stack.length - 1);
+        return true;
+
+    }
+
+    public peek(): itemType {
+
+        return this.m_stack[this.m_stack.length - 1];
+
+    }
+            
+}
